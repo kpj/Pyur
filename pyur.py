c4cf497dff72bae957c0f9c5477d0bf2a3e2713f
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2012-01-25 16:11:23 +0100

    first commit

---
+++ pyur.py
@@ -0,0 +1,102 @@
+import urllib, urllib.request, json, sys
+
+class aur(object):
+	def __init__(self):
+		self.cu = core_utils()
+		self.curl = self.cu.curl
+		self.ta = text_attributes()
+
+		self.base_url = "http://aur.archlinux.org"
+		self.com_url = "%s/rpc.php?type=%s&arg=%s" % (self.base_url, "%s", "%s")
+
+		self.working_dir = "/tmp"
+
+		self.search_arg = "search"
+
+	def handle_start(self):
+		todo, data = self.cu.parse_input(sys.argv)
+		todo = "%s%s" % (todo, "      ")
+		if todo[0] == "S":
+			if todo[1] == "s":
+				for d in data:
+					self.show_search(self.search_pattern(d))
+			if todo[1] == " ":
+				for d in data:
+					self.install_pattern(d)
+
+	def search_pattern(self, pattern):
+		x = self.curl(self.com_url % (self.search_arg, pattern) )
+		return json.loads(x.decode("utf-8"))
+
+	def install_pattern(self, pattern):
+		print(self.ta.s(["bold","white"]) + "Download: " + self.ta.s(["blue"]) + pattern)
+		x = self.curl(self.com_url % (self.search_arg, pattern) )
+		x = json.loads(x.decode("utf-8"))
+		n = "%s%s" % (x["results"][0]["Name"], ".tar.gz")
+		dl = x["results"][0]["URLPath"] # First result fits best
+		dl_url = "%s%s" % (self.base_url, dl)
+		self.cu.download(dl_url, "%s/%s" % (self.working_dir, n))
+
+		print(self.ta.s(["bold","white"]) + "Install: " + self.ta.s(["blue"]) + pattern)
+		print("...")
+
+	def show_search(self, dd):
+		items = dd["results"]
+		for item in items:
+			name = item["Name"]
+			version = item["Version"]
+			desc = item["Description"]
+			print(self.ta.s(["blue", "bold"]) + name + " " + self.ta.s(["green"]) + "(" + version +")")
+
+
+class core_utils(object):
+	def __init__(self):
+		pass
+
+	def curl(self, url):
+		c = urllib.request.Request(url)
+		code = urllib.request.urlopen(c).read()
+		return code
+
+	def download(self, url, f):
+		try:
+			urllib.request.urlretrieve(url, f)
+		except ContentTooShortError:
+			print("There was an error when saving your file...")
+			sys.exit()
+
+	def parse_input(self, inp):
+		if len(inp) == 1:
+			print("Exiting with no arguments given...")
+			sys.exit()
+		do = sys.argv[1]
+		if do[0] != "-":
+			print("First argument has to define action[s]")
+			sys.exit()
+		do = do[1:]
+		# "do" contains what to do, e.g. "S", "Ss", ...
+		return do, sys.argv[2:]
+
+class text_attributes(object):
+	def __init__(self):
+		self.base = '\033[%sm'
+
+		self.translate = {
+			# Styles
+			"normal":0,
+			"bold":1,
+			# Colors
+			"red":31,
+			"blue":34,
+			"green":32,
+			"white":37,
+		}
+
+	def s(self, args):
+		a = ""
+		for v in args:
+			a += "%s;" % self.translate[str(v)]
+		a = a[:-1] # Remove last ";"
+		return self.base % "0" + self.base % a
+
+aur().handle_start()
